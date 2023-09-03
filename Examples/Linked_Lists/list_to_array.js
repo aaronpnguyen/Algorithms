e649685ff27de781573b3225f1f32ae39ad7b34a
aaronpnguyen <aaronnguyen1123@gmail.com>
2023-09-03 15:40:49 -0500

    Intro to linked list

---
+++ Examples/Linked_Lists/list_to_array.js
@@ -0,0 +1,61 @@
+// Given a linked list, convert the list into an array
+
+// Create the structure for a node
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+// Initializing a group of nodes
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+
+// Linking the nodes together
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+// const listToArray = (head) => {
+//     // Start with an empty array, because we should not have any values yet
+//     let convertedArray = [];
+
+//     // Traversing iteratively
+//     while (head !== null) {
+//         convertedArray.push(head.value);
+
+//         head = head.next;
+//     }
+
+//     return convertedArray;
+// }
+
+const listToArray = (head) => {
+    const array = [];
+
+    insertValue(head, array);
+
+    return array
+}
+
+const insertValue = (head, array) => {
+    if (head == null) {
+        return
+    }
+
+    // Add value into array that is passed in
+    array.push(head.value);
+
+    // Traversing recursively
+    insertValue(head.next, array);
+
+    return array
+}
+
+
+console.log(listToArray(a))
